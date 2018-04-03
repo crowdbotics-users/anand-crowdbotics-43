5773bb55d12f366bd2f419f8dd705e371ebc179e
Saurav <sp.gharti@gmail.com>
2018-04-03 23:46:24 +0000

    add package django4facebook

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django4facebook', 'url': 'http://pypi.python.org/pypi/django4facebook/0.1.0'},
     ]
     context = {
         'title': 'anand-crowdbotics-43',
